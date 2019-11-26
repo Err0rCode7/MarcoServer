d6a39ff016528cbb7f3cfa0e874e4da86f782d04
SW <aa6184708@naver.com>
2019-11-27 06:20:31 +0900

    First File

---
+++ Marco/Marco_main/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'Marco_main'
+
+urlpatterns = [
+    url('api-auth/', include('rest_framework.urls')),
+    url(r'^$', views.Marco_main.as_view(), name='Marco'),
+    #url(r'^todo_list/$', views.Todo_subject_restful_main.as_view(), name='todo_list'),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Marco/Marco_main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse# Create your views here.
+from django.views import View
+from django.views import generic
+
+class Marco_main(generic.TemplateView) :
+    template_name = 'marco_main/main_view.html'
+    def get(self, request, *args, **kwargs):
+        my_Sensor = '1111'
+        return render(request, self.template_name, { 'Sensor' : my_Sensor})
+
+    def post(self, request, *args, **kwargs):
+        #센서 ON
+
+        #센서 off
+
+        image_url = 'static/gif/testimg3.gif'
+        image_src = "http://127.0.0.1:8000/" + image_url
+        text_content = "안녕하세요 ㅎㅎㅎ"
+        data = {'Image': image_url, 'src': image_src, 'text':text_content }
+        return JsonResponse(data)
+
+
+    #if request.method == "POST":
+
+
